ff8c87c8c2779f30e0a5102bd3e2fce8e26c5045
Anisah-hash <anysahani125@gmail.com>
2022-05-16 12:31:41 -0700

    split

--- split.c
+++ split.c
@@ -5,12 +5,12 @@ char **_strtok(char *line, char *delim);
 
 /**
  * token_len - Locates the delimiter index marking the end
- *             of the first token contained within a string.
- * @str: The string to be searched.
- * @delim: The delimiter character.
+ *             of the first token contained within a string
+ * @str: The string to be searched
+ * @delim: The delimiter character
  *
  * Return: The delimiter index marking the end of
- *         the intitial token pointed to be str.
+ *         the intitial token pointed to be str
  */
 int token_len(char *str, char *delim)
 {
